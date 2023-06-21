71822d93d05b37dac05e754bec55ea9c03884bee
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2023-06-21 22:08:23 +0530

    spring boot sso SAML2.0

---
+++ spring-boot-sso/springboot-sso-saml/src/main/java/com/ece/ssosaml/SpringbootSsoSamlApplication.java
@@ -0,0 +1,13 @@
+package com.ece.ssosaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSsoSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSsoSamlApplication.class, args);
+	}
+
+}
---
+++ spring-boot-sso/springboot-sso-saml/src/test/java/com/ece/ssosaml/SpringbootSsoSamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.ssosaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSsoSamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
